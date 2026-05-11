f96c5a93fe60390fb57698ad44ff7351b8742e71
sulhaSabn <95939206+sulhaSabn@users.noreply.github.com>
2026-05-11 11:06:49 +0430

    Delete User.js

--- User.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const UserSchema = new mongoose.Schema({
-  email: { type: String, unique: true },
-  password: String,
-  balance: { type: Number, default: 0 }
-});
-
-module.exports = mongoose.model("User", UserSchema);
